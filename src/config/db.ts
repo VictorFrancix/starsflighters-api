735c6d5ef7222a028ad4f98cc0bb4cd157387a66
João Victor <jvictor.franci@gmail.com>
2022-07-07 17:46:59 -0300

    final

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,7 +6,8 @@ dotenv.config();
 const {Pool} = pg;
 
 const connection = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgres://postgres:74762510@localhost:5432/starfighters'
+    connectionString: process.env.DATABASE_URL
 });
+
 
 export default connection;
